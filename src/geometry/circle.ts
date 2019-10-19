cd95e0f768d3ede55cc22dcb017c487fc3c8e722
Jordan Broome <jbroome@sitepro.com>
2019-10-19 17:41:09 -0500

    adds circumference function

--- src/geometry/circle.ts
+++ src/geometry/circle.ts
@@ -10,5 +10,14 @@ export const circle = {
     if (radius < 0) throw new Error('Radius must be greater than 0!');
     if (typeof radius !== 'number') throw new Error('Radius must be a number!');
     return Math.PI * radius * radius;
+  },
+  /**
+   * Calculate the circumference of a circle with a given diameter.
+   * @param diameter
+   */
+  circumference(diameter: number) {
+    if (diameter < 0) throw new Error('Radius must be greater than 0!');
+    if (typeof diameter !== 'number') throw new Error('Diameter must be a number!');
+    return Math.PI * diameter;
   }
 };
