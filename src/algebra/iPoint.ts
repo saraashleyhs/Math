e4d01ce0ea086b33eacd9232fff7f443e5e966f0
saraashleyhs <sara.ashleyhs@gmail.com>
2019-10-20 00:13:17 -0500

    Created Distance, Midpoint,point interface

---
+++ src/algebra/iPoint.ts
@@ -0,0 +1,6 @@
+export interface iPoint
+{
+    x : number;
+    y : number;
+}
+
--- src/algebra/index.ts
+++ src/algebra/index.ts
@@ -1,3 +1,4 @@
+import {iPoint} from './iPoint'; 
 export const algebra = {
   subtract(a: number, b: number): number {
     return a - b;
@@ -12,5 +13,17 @@ export const algebra = {
     const m = (pointY2 - pointY1) / (pointX2 - pointX1);
     return m;
   },
-
+  midpoint(point1:iPoint, point2:iPoint):iPoint{
+    const medX = (point1.x+point2.x)/2;
+    const medY = (point1.y+point2.y)/2;
+    let midPoint:iPoint = {
+      x: medX,
+      y: medY
+    };
+    return midPoint;
+  },
+  distance(point1:iPoint, point2:iPoint):number{
+    const dist = Math.sqrt((point1.x-point2.x)**2+(point1.y-point2.y)**2);
+    return dist;
+  }
 };
