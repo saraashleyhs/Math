ff796badf365256e06fc872aa6634ebf6389d8b2
Johnny Reina <johnny.r.reina@gmail.com>
2019-10-19 19:11:57 -0500

    Fixes stuff

--- src/algebra/index.ts
+++ src/algebra/index.ts
@@ -1,9 +1,15 @@
 export const algebra = {
-  slope,
-  subtract(a:number, b:number): number {
-      return a - b;
+  subtract(a: number, b: number): number {
+    return a - b;
   },
   add(a: number, b: number): number {
     return a + b;
+  },
+  /**
+   * function for finding the slope of two points.
+   */
+  slope(pointX1: number, pointY1: number, pointX2: number, pointY2: number) {
+    const m = (pointY2 - pointY1) / (pointX2 - pointX1);
+    return m;
   }
 };
--- src/algebra/slope.ts
+++ src/algebra/slope.ts
@@ -1,9 +1,1 @@
-export const slope = {
-    /**
-     * function for finding the slope of two points.
-     */
-   slope(pointX1: number, pointY1: number, pointX2: number, pointY2: number) {
-       const m = ((pointY2 - pointY1) / (pointX2 - pointX1));
-       return m;
-   }
-}
+export const slope = {};
